66c8095b827d32d432a1e4c6439856ed67381362
Inbisat <a23inbnavnav@inspedralbes.cat>
2025-03-10 12:02:45 +0100

    correcting errors

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devServer: {
+    port: 3000,  
+    host: '0.0.0.0' 
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, 
+      }
+    }
+  }
 })
