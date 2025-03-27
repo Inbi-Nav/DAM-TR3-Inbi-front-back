f02aeb0a657b90c731b7da0163f8cec68f9dbebf
Inbisat Naveed <inkyn2002@gmail.com>
2025-03-27 19:10:26 +0100

    registrar partidas usuario

--- backend/microservices/auth_service/routes/userRoutes.js
+++ backend/microservices/auth_service/routes/userRoutes.js
@@ -29,6 +29,22 @@ router.post('/login', async (req, res) => {
   res.status(200).json({ message: 'Login successful', userId: user.id });
 });
 
+router.patch('/:id/gamesPlayed', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ error: 'Usuario no encontrado' });
+
+    user.gamesPlayed += 1;
+    await user.save();
+    res.json({ message: 'Partida registrada', gamesPlayed: user.gamesPlayed });
+  } catch (err) {
+    res.status(500).json({ error: 'Error al registrar partida' });
+  }
+});
+
+
+
 router.get('/', async (req, res) => {
   const users = await User.findAll();
   res.json(users);
